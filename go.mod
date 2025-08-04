a65bfac98f40d0e6a8d6ca8fc9e26f6005a7bded
Jack Bennett <jackbennett0708@gmail.com>
2025-08-04 18:10:39 +0100

    Setup go project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module gitlab.com/kosude/cards
+
+go 1.24.5
+
+require github.com/go-chi/chi/v5 v5.2.2
---
+++ main.go
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2025 Jack Bennett.
+ * All Rights Reserved.
+ *
+ * See the LICENCE file for more information.
+ */
+
+package main
+
+import "gitlab.com/kosude/cards/app"
+
+func main() {
+	app.NewApp().Run()
+}
