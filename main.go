1dad0ca4269ccac3432f68ef193929bfb8ed0746
Jack Bennett <jackbennett0708@gmail.com>
2025-08-05 17:25:42 +0100

    Set up Gitlab CI to push to webserver

--- main.go
+++ main.go
@@ -8,13 +8,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 	"net/http"
 )
 
 func main() {
-	addr := net.JoinHostPort("::", "8100")
-	server := &http.Server{Addr: addr}
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /test", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "testing!!")
+	})
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "root (working???)")
+	})
+
+	server := &http.Server{
+		Addr:    net.JoinHostPort("::", "8100"),
+		Handler: mux,
+	}
+
+	println("Newest version")
+
 	log.Fatalln(server.ListenAndServe())
 }
